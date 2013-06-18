0c7b710bd70a966237400ff29d3e34651fe58a07
jbcazaux <jbcazaux@valtech-pc.(none)>
2013-06-18 10:54:06 +0200

    constructor sample

---
+++ js/samples/2-constructor.js
@@ -0,0 +1,21 @@
+var User = function(name, login){
+    this.name = name;
+    this.login = login;
+    this.coeff = 3.2;
+
+    this.computeSomething = function(p){
+        return p * this.coeff;
+    };
+
+    var privateCoeff = 1.1;
+    this.getPrivateCoeff = function(){
+        return privateCoeff;
+    };
+
+    //return this;
+};
+
+var u1 = new User("toto", "toto1");
+console.log("coeff: " + u1.coeff);
+console.log("private coeff: " + u1.privateCoeff);
+console.log("get private coeff: " + u1.getPrivateCoeff());
