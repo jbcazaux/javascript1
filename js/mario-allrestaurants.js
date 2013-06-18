be96cc9747326c7326aa80f92911e8e14ecd4172
jbcazaux <jbcazaux@gmail.com>
2013-06-18 17:28:50 +0200

    TP1: add solution

---
+++ js/mario-allrestaurants.js
@@ -0,0 +1,15 @@
+(function($, mario){
+
+    $(function(){
+        $.get('/mario/data/restaurants.json', function(data){
+            $.each(data, displayRestaurant);
+        });
+    });
+
+    function displayRestaurant(i, restaurant){
+        var link = mario.createLink('restaurant.html?r=' + restaurant.id, restaurant.name);
+        var li = $('<li></li>').html(link);
+        $('.pizzerias ul').append(li);
+    }
+
+})(jQuery, mario);
